37b0e685e56991804568936d9e0da790c6576b95
Jose <Sgtjosetorres@yahoo.com>
2020-10-14 08:35:00 -0500

    added properties to Address

--- LoveWedLive_Capstone/Models/Address.cs
+++ LoveWedLive_Capstone/Models/Address.cs
@@ -7,6 +7,11 @@ namespace LoveWedLive_Capstone.Models
 {
     public class Address
     {
-        
+        int AddressId;
+        string StreetName;
+        string City;
+        string State;
+        int Zipcode;
+
     }
 }
