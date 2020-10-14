ada3b6f5cdf12e09313ad33d4175ae7d4601978d
Jose <Sgtjosetorres@yahoo.com>
2020-10-14 08:30:47 -0500

    added classes

---
+++ LoveWedLive_Capstone/Models/VendorQuotes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoveWedLive_Capstone.Models
+{
+    public class VendorQuotes
+    {
+    }
+}
